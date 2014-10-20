619bc5f2f4c33f2a9a8a51633ecc331a43c00edb
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-10-21 03:46:34 +0530

    Implemented Activity for the whole project that glue all things together

--- src/com/techsect/HWeather/WeatherActivity.java
+++ src/com/techsect/HWeather/WeatherActivity.java
@@ -1,7 +1,13 @@
 package com.techsect.HWeather;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class WeatherActivity extends Activity {
     /**
@@ -10,6 +16,39 @@ public class WeatherActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_weather);
+
+        if(savedInstanceState == null){
+            getFragmentManager().beginTransaction().add(R.id.container, new WeatherFragment()).commit();
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.change_city){
+            showInputDialog();
+        }
+        return false;
+    }
+
+    private void showInputDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Change City");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                changeCity(input.getText().toString());
+            }
+        });
+        builder.show();
+    }
+
+    public void changeCity(String city){
+        WeatherFragment wf = (WeatherFragment)getFragmentManager().findFragmentById(R.id.container);
+        wf.changeCity(city);
+        new CityPreference(this).setCity(city);
     }
 }
