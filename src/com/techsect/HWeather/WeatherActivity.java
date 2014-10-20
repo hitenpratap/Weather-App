12f2c366271d44e89eaad1a98fdea33dabb74989
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-10-21 01:20:30 +0530

    Initial commit

---
+++ src/com/techsect/HWeather/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.techsect.HWeather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
