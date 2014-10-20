7832db305a9f1a4408eb07bd124d7478b2047817
Hiten Pratap Singh <hitenpratap99@gmail.com>
2014-10-21 02:45:07 +0530

    Implemented code to store selected city into SharedPreferences

---
+++ src/com/techsect/HWeather/CityPreference.java
@@ -0,0 +1,27 @@
+package com.techsect.HWeather;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by hitenpratap on 21/10/14.
+ */
+public class CityPreference {
+
+    SharedPreferences preferences;
+    private static final String SHARED_PREF_CITY_KEY_NAME = "CITY";
+
+    public CityPreference(Activity activity){
+        preferences = activity.getPreferences(Context.MODE_PRIVATE);
+    }
+
+    String getCity(){
+        return preferences.getString(SHARED_PREF_CITY_KEY_NAME,"New York,US");
+    }
+
+    void setCity(String city){
+        preferences.edit().putString(SHARED_PREF_CITY_KEY_NAME,city);
+    }
+
+}
